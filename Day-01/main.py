5da6ed024be44fc2e42268a374b4efe226ce5a07
Safa Ustad <safa.e.ustad@gmail.com>
2026-06-25 10:22:16 +0530

    Create main.py

---
+++ Day-01/main.py
@@ -0,0 +1,11 @@
+# We are to provide a Band Name 
+print("Welcome to the Band Name Generator.") 
+
+# input their City Name
+city = input("Which city did you grow up in?\n")
+
+# input their Pet Name
+pet = input("What is the name of a pet?\n")
+
+# Band name = City + Pet Name
+print("Your band name could be " + city + " " + pet)
